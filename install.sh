7fbd0edd2ba809b858cb4e07c8acbc66c1ce7c5a
Matt Widmann <mgwidmann@gmail.com>
2022-05-18 21:15:44 -0400

    Fix for issue with not updating background because of the same file name used.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/.bing-image-of-the-day
 cp -R src/ ~/.bing-image-of-the-day/
 
-(crontab -l 2>/dev/null; echo "0 12 * * * ~/.bing-image-of-the-day/change-desktop.sh") | crontab -
+(crontab -l 2>/dev/null; echo "5 15 * * * ~/.bing-image-of-the-day/change-desktop.sh") | crontab -
--- src/change-desktop.sh
+++ src/change-desktop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Move the current image to yesterday's file and change to that desktop so that the new will register as a change instead of the same file again
+mv ~/.bing-image-of-the-day/wallpaper.jpeg ~/.bing-image-of-the-day/yesterday.jpeg
+osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"/Users/$(whoami)/.bing-image-of-the-day/yesterday.jpeg\""
+
+# Download the latest image as wallpaper.jpeg
 curl -s https://www.bing.com/HPImageArchive.aspx\?format\=js\&idx\=0\&n\=1\&mkt\=en-US | jq -r '.images[0] | "https://www.bing.com" + .url' | xargs curl -s -o ~/.bing-image-of-the-day/wallpaper.jpeg
 
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"/Users/$(whoami)/.bing-image-of-the-day/wallpaper.jpeg\""
